faa27455a1cb5a314df7f189e820b9ccb8b5d5be
seoyeon0413 <sally9476@naver.com>
2020-08-04 15:14:24 +0900

    - problem 1 correction - problem 2 solving

--- problem1/problem1_수정.py
+++ problem1/problem1_수정.py
@@ -1,81 +1,122 @@
-import string
 from tkinter import *
 import pandas as pd
+import yaml
 
 
-class Problem1:
+class Remove_Special_Character_Program:
 
     def __init__(self):
         pass
-
-    def remove_specialCharacter(self, text):
-        '''
-        remove punctuation :https://towardsdatascience.com/nlp-for-beginners-cleaning-preprocessing-text-data-ae8e306bef0f
-        :param text: text data with dataframe type
-        :return: text removed special characters
-        '''
-
-        no_sc = "".join([c for c in text if c not in string.punctuation])
-        return no_sc
 
     def input_remove_specialCharacter(self, text, sc):
         '''
         get input punctuation and remove them
         :param text: text data with dataframe type
         :param sc: special characters getting from input
-        :return: text removed special characters
+        :return:text with no special characters
         '''
 
         sc += '\n'
-        ### list comprehension외에 다른 방식으로 처리할 수 있는지 고민할 것
         no_sc = "".join([c for c in text if c not in sc])
         return no_sc
 
     def replace_specialCharacter(self, text):
         '''
-        replace punctuation to spacebar character
+        replace special character to spacebar character
         :param text: text data with dataframe type
-        :return: text replaced special characters to spacebar character
+        :return: text with replacing special characters to spacebar character
         '''
 
-        """
-        punc = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'
-        no_punct = re.sub("[^A-Za-z0-9-' '-'\'-\'-']+", ' ', text)
-        punc = string.punctuation + "'" + '-' + ','
-        punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-        no_punct = " ".join(word.strip(punc) for word in text.split())
-        print(no_punct)
-        no_punct = re.sub(punc, ' ', text)
-        no_punct = text.translate({ord(x): y for (x, y) in zip(punc, len(punc)*' ')})
-        """
         no_sc = re.sub(r'[^\w\s]', ' ', text)
-        no_sc = re.sub(r'\_', '', no_sc)
+        no_sc = re.sub(r'\_', ' ', no_sc)
         no_sc = re.sub(r'\n', '', no_sc)
 
         return no_sc
 
-    def getResultOption(self):
+    def split_data(self, rawData):
         '''
-        input file name and extension option
-        :return: file name and extension option
+        get document number & remove doc number in text
+        :param rawData: data from file
+        :return: data converted to dataframe type
         '''
 
-        fileName = input("[file name with extension]\n"
-                         "(extension: csv, txt, json)\n")
-        fName = fileName.split('.')[0]
-        extension = fileName.split('.')[1]
-        print(fName, extension)
+        fNum = []
+        index = 0
+        for line in rawData:
+            if line.split(' ')[0].isdigit() == True:
+                fNum.append(int(line.split(' ')[0]))
+                rawData[index] = line.replace(line.split(' ')[0], '')
+                index += 1
 
-        return fName, extension
+        data = {'docNum': fNum, 'text': rawData}
+        data = pd.DataFrame(data)
 
-    def writeData(self, data):
+        return data
+
+    def text_processing(self, propertyData, data):
         '''
-        save results by extension option
+        text processing function (remove special characters from data)
+        :param propertyData: data from property file
+        :param data: data with dataframe type
+        :return: 'processed data', 'output file name'
+        '''
+
+        scOption = propertyData['option']
+        if (scOption == 'replace'):
+            specialChar = propertyData['special characters']
+            outputFileName = propertyData['output']
+            data['text'] = data['text'].apply(lambda x: self.input_remove_specialCharacter(x, specialChar))
+        elif (scOption == 'remove'):
+            outputFileName = propertyData['output']
+            data['text'] = data['text'].apply(lambda x: self.replace_specialCharacter(x))
+        else:
+            print("\'option\'에 \'remove\' (이)나 \'replace\' 를 입력해야합니다.")
+            sys.exit()
+
+        return data, outputFileName
+
+    def get_data(self, propertyData):
+        '''
+        get data from input file using property data
+        :param propertyData: data from property file
+        :return: data from input file
+        '''
+        try:
+            fileName = propertyData['input']
+        except IndexError as e:
+            print("property 파일에 input을 써주세요:{}".format(e))
+        try:
+            with open(fileName, 'r') as file:
+                dataFromFile = file.readlines()
+        except FileNotFoundError as e:
+            print('해당하는 파일이 없습니다: {}.format'.format(e))
+
+        return dataFromFile
+
+    def get_property(self):
+        '''
+        get data from property file
+        :return: data from property file
+        '''
+
+        try:
+            with open("property.yml") as file:
+                propertyData = yaml.load(file, Loader=yaml.FullLoader)
+        except FileNotFoundError as e:
+            print('해당하는 파일이 없습니다: {}'.format(e))
+
+        return propertyData
+
+    def write_data(self, data, outFName):
+        '''
+        output file name and extension option to save results by extension option
         :param data: results that have to save
         :return: nothing
         '''
 
-        fn, ext = self.getResultOption()
+        fn = outFName.split('.')[0]
+        ext = outFName.split('.')[1]
+
         if (ext == "csv"):  # result to csv
             data.to_csv(fn + '.csv', index=False, header=['docNum', 'text'])
         elif (ext == "txt"):    # result to txt
@@ -87,56 +128,7 @@ class Problem1:
             data.to_json(fn, orient='table')
         else:
             print("Error: Write Another Extension (csv, txt, json)")
-            self.writeData(data)
-
-    def getInputName(self):
-        '''
-        get input file name
-        :return: file name
-        '''
-
-        # fname, ext = os.path.splitext('input.txt')
-        # print(fname)
-        # print(ext)
-        # fileName = fname + ext
-        fileName = input("- Input file name with extension: ")
-
-        return fileName
-
-    def readFile(self, fn):
-        '''
-        read text file from directory
-        :param fn: file name
-        :return: data from file
-        '''
-
-        try:
-            with open(fn, 'r') as file:
-                readAllFile = file.readlines()
-        except FileNotFoundError as e:
-            print('해당하는 파일이 없습니다: {}.format'.format(e))
-
-        return readAllFile
-
-    def splitData(self, d):
-        '''
-        get document number & remove doc number in text
-        :param d: data from file
-        :return: data converted to dataframe type
-        '''
-
-        fNum = []
-        index = 0
-        for line in d:
-            if line.split(' ')[0].isdigit() == True:
-                fNum.append(int(line.split(' ')[0]))
-                d[index] = line.replace(line.split(' ')[0], '')
-                index += 1
-
-        data = {'docNum': fNum, 'text': d}
-        data = pd.DataFrame(data)
-
-        return data
+            self.write_data(data, outFName)
 
     def run(self):
         '''
@@ -144,29 +136,15 @@ class Problem1:
         :return: nothing
         '''
 
-        fileName = self.getInputName()
-        rf = self.readFile(fileName)
-        data = self.splitData(rf)
+        propertyData = self.get_property()
+        dataFrame = self.get_data(propertyData)
+        data = self.split_data(dataFrame)
+        processedData, outputFileName = self.text_processing(propertyData, data)
+        self.write_data(processedData, outputFileName)
 
-        ### 솔루션에서는 키보드 입력보다는 property 파일을 만들어서 실행 시키는 것이 좋음
-        ### 키보드 입력받으면 중간중간 사용자가 입력을 해 줘야 하기 때문에 interupt가 걸림
-        punc = int(input("1 - punctuation directly setting (Remove punctuation)\n"
-                         "2 - existed punctuation usage (Replace to spacebard)\n"))
-
-        if (punc == 1):
-            punc = input("[Punctuation]\n"
-                         "(just write each punctuation without spacebar and write Enter key when you are done)\n")
-            data['text'] = data['text'].apply(lambda x: self.input_remove_specialCharacter(x, punc))
-        elif (punc == 2):
-            data['text'] = data['text'].apply(lambda x: self.replace_specialCharacter(x))
-
-        # data['text'] = data['text'].apply(lambda x: self.remove_specialCharacter(x))
-        # print(data)
-        print(data)
-
-        self.writeData(data)
+        print("Program Run Success!")
 
 
 if __name__ == "__main__":
-    p = Problem1()
-    p.run()
+    removeSpecialCharacterProgram = Remove_Special_Character_Program()
+    removeSpecialCharacterProgram.run()
